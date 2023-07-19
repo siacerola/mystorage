57ab47b218901c5c7cdaeb876f46100cc4b73501
sihar <luq5nvd@gmail.com>
2023-07-19 14:54:14 +0700

    create user model

---
+++ src/app/model/user.model.ts
@@ -0,0 +1,18 @@
+export class UserModel{
+  id!: number
+  email!: string
+  password!: string
+  role!: string
+
+  constructor(
+    id: number,
+    email: string,
+    password: string,
+    role:string
+  ) {
+    this.id = id
+    this.email = email
+    this.password = password
+    this.role=role
+  }
+}
