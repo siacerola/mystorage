9b32a3774f972e8a1e43dc7d86bbe00f9db71f7a
sihar <luq5nvd@gmail.com>
2023-07-24 20:02:03 +0700

    create chart page

--- src/app/pages/chartline/chartline.component.ts
+++ src/app/pages/chartline/chartline.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from 'ng-apexcharts'
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries
+  chart: ApexChart
+  xaxis: ApexXAxis
+  title: ApexTitleSubtitle
+};
+
 @Component({
   selector: 'app-chartline',
   templateUrl: './chartline.component.html',
   styleUrls: ['./chartline.component.css']
 })
 export class ChartlineComponent {
+  chartOptions: Partial<ChartOptions>;
+
+  constructor() {
+    this.chartOptions = {
+      series: [{
+        name: "Products",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+      }],
+      chart: {
+        height: 350,
+        type:"line"
+      },
+      title: {
+        text:"Finish Product annually"
+      },
+      xaxis: {
+        categories:[2014, 2015,  2016,  2017,  2018,  2019,  2020,  2021, 2022]
+      }
+    }
+  }
 
 }
