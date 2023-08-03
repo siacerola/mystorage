233b7f85e92d5e24aeb45c09ba51dfa0e018a651
siacerola <si.acerola@gmail.com>
2023-08-03 14:11:09 +0700

    create heading componen

---
+++ src/app/component/heading/heading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heading',
+  templateUrl: './heading.component.html',
+  styleUrls: ['./heading.component.css']
+})
+export class HeadingComponent {
+  title = "heading"
+  subtitle="detail heading"
+
+}
