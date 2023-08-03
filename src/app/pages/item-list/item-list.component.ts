dabc2400c8cc3c111800a4841a59b7986fafcce6
siacerola <si.acerola@gmail.com>
2023-08-03 09:16:37 +0700

    add item to itemModel

--- src/app/pages/item-list/item-list.component.ts
+++ src/app/pages/item-list/item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ItemModel } from '../../model/item.model';
 
 @Component({
   selector: 'app-item-list',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent {
+  itemTang: ItemModel = new ItemModel({
+    id: 1,
+    itemName: "Tang Kombinasi",
+    category: "developer",
+    qty: 1,
+    status:"available"
+  })
+
+  itemObeng: ItemModel = new ItemModel({
+    id: 2,
+    itemName: "obeng impact +",
+    category: "developer",
+    qty: 1,
+    status:"not available"
+  })
+
+  itemList: ItemModel[] = [
+    this.itemTang,
+    this.itemObeng
+  ]
 
 }
