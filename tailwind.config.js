59ecb1e5f264fd6f1c8abd4f4e067c89c47480bc
siacerola <si.acerola@gmail.com>
2023-08-09 11:25:47 +0700

    import theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
     "./src/**/*.{html,js}"
   ],
   theme: {
+    colors: {
+      'white-blue': '#3F72AF',
+      'dark-blue': '#112D4E',
+      'gray-blue': '#DBE2EF',
+      'gray-slate':'#F9F7F7'
+    },
     extend: {},
   },
+
   plugins: [
   ]
 }
