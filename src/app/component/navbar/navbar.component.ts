34537fde04cda4aedc3864c03d8544be89f0dd7c
siacerola <si.acerola@gmail.com>
2023-08-09 16:14:52 +0700

    con

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  title = "MYSTORAGE"
+  faDatabase = faDatabase
 }
