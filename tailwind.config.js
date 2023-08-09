de1111eab49e16692a5847649f7c691208ea338f
siacerola <si.acerola@gmail.com>
2023-08-09 10:56:11 +0700

    remove flowbit dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js"
+    "./src/**/*.{html,js}"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
   ]
 }
