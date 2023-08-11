5b31ea6826faa22f6746e79363f643cccb26466e
siacerola <si.acerola@gmail.com>
2023-08-11 16:06:00 +0700

    line chart display

--- src/app/component/line-chart/line-chart.component.ts
+++ src/app/component/line-chart/line-chart.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
 
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexStroke,
+  ApexLegend,
+  ApexMarkers
+} from 'ng-apexcharts'
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries
+  chart: ApexChart
+  xaxis: ApexXAxis
+  stroke: ApexStroke
+  legend: ApexLegend
+  markers: ApexMarkers
+  dataLabels:ApexDataLabels
+}
+
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
   styleUrls: ['./line-chart.component.css']
 })
 export class LineChartComponent {
+
+  title = "Monthly Sales"
+  subtitle=""
+
+  chartOptions :Partial<ChartOptions>
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          data: [57, 56, 61, 58, 63, 60, 66]
+        },
+      ],
+      chart: {
+        type: 'line',
+        height: 350,
+        toolbar: {
+          show:false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve:'stepline',
+        show: true,
+        width: 2,
+        colors: ['transparent']
+      },
+      xaxis: {
+        categories: ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+      }
+    }
+  }
 
 }
--- src/app/component/three-line-chart/three-line-chart.component.ts
+++ src/app/component/three-line-chart/three-line-chart.component.ts
@@ -1,25 +1,5 @@
 import { Component } from '@angular/core';
 
-import {
-  ApexAxisChartSeries,
-  ApexChart,
-  ApexXAxis,
-  ApexDataLabels,
-  ApexStroke,
-  ApexLegend,
-  ApexMarkers
-} from 'ng-apexcharts'
-
-export type ChartOptions = {
-  series: ApexAxisChartSeries
-  chart: ApexChart
-  xaxis: ApexXAxis
-  stroke: ApexStroke
-  legend: ApexLegend
-  markers: ApexMarkers
-  dataLabels:ApexDataLabels
-}
-
 @Component({
   selector: 'app-three-line-chart',
   templateUrl: './three-line-chart.component.html',
@@ -27,45 +7,5 @@ export type ChartOptions = {
 })
 export class ThreeLineChartComponent {
 
-  title = "Cash Flow"
-  subtitle=""
 
-  chartOptions :Partial<ChartOptions>
-
-  constructor() {
-    this.chartOptions = {
-      series: [
-        {
-          name: 'Net Profit',
-          data: [57, 56, 61, 58, 63, 60, 66]
-        },
-        {
-          name: 'Revenue',
-          data: [ 101, 98, 87, 105, 91, 114, 94]
-        },
-        {
-          name: 'Free Cash Flow',
-          data: [36, 26, 45, 48, 52, 53, 41]
-        }
-      ],
-      chart: {
-        type: 'bar',
-        height: 350,
-        toolbar: {
-          show:false
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        show: true,
-        width: 2,
-        colors: ['transparent']
-      },
-      xaxis: {
-        categories: ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-      }
-    }
-  }
 }
