e32a73a31a40799dfbdcd4675235bc3106e2579e
siacerola <si.acerola@gmail.com>
2023-08-11 09:24:02 +0700

    layouting dashboard chart

--- src/app/component/main-chart/main-chart.component.ts
+++ src/app/component/main-chart/main-chart.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
 
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexStroke,
+  ApexLegend,
+  ApexMarkers
+} from 'ng-apexcharts'
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries
+  chart: ApexChart
+  xaxis: ApexXAxis
+  stroke: ApexStroke
+  legend: ApexLegend
+  markers: ApexMarkers
+  dataLabels:ApexDataLabels
+};
+
 @Component({
   selector: 'app-main-chart',
   templateUrl: './main-chart.component.html',
   styleUrls: ['./main-chart.component.css']
 })
 export class MainChartComponent {
+
+  title = "Finish Product"
+  subtitle = "Finish product annually"
+
+  chartOptions: Partial<ChartOptions>;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Roaster 3kg",
+          data: [10, 20, 15, 18, 23, 47, 31, 56, 61]
+        },
+        {
+          name: "Roaster 5kg",
+          data: [5, 6, 11, 13, 9, 12, 14, 25, 36]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "area",
+        toolbar: {
+          show:false
+        }
+      },
+      dataLabels: {
+        enabled:false
+      },
+      markers: {
+        size: 5,
+      },
+      xaxis: {
+        categories:[2014, 2015,  2016,  2017,  2018,  2019,  2020,  2021, 2022]
+      }
+    }
+  }
 
 }
--- src/app/component/three-line-chart/three-line-chart.component.ts
+++ src/app/component/three-line-chart/three-line-chart.component.ts
@@ -37,15 +37,15 @@ export class ThreeLineChartComponent {
       series: [
         {
           name: 'Net Profit',
-          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+          data: [57, 56, 61, 58, 63, 60, 66]
         },
         {
           name: 'Revenue',
-          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+          data: [ 101, 98, 87, 105, 91, 114, 94]
         },
         {
           name: 'Free Cash Flow',
-          data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+          data: [36, 26, 45, 48, 52, 53, 41]
         }
       ],
       chart: {
@@ -64,7 +64,7 @@ export class ThreeLineChartComponent {
         colors: ['transparent']
       },
       xaxis: {
-        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        categories: ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
       }
     }
   }
