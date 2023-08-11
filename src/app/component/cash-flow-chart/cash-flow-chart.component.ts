e29f717b3580f56a841e99c6d7ef43d553bc39c6
siacerola <si.acerola@gmail.com>
2023-08-11 09:27:49 +0700

    move chart to cash flow chart

--- src/app/component/cash-flow-chart/cash-flow-chart.component.ts
+++ src/app/component/cash-flow-chart/cash-flow-chart.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
 
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexStroke,
+  ApexLegend,
+  ApexMarkers
+} from 'ng-apexcharts'
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries
+  chart: ApexChart
+  xaxis: ApexXAxis
+  stroke: ApexStroke
+  legend: ApexLegend
+  markers: ApexMarkers
+  dataLabels:ApexDataLabels
+}
 @Component({
   selector: 'app-cash-flow-chart',
   templateUrl: './cash-flow-chart.component.html',
   styleUrls: ['./cash-flow-chart.component.css']
 })
 export class CashFlowChartComponent {
+  title = "Cash Flow"
+  subtitle=""
 
+  chartOptions :Partial<ChartOptions>
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: 'Net Profit',
+          data: [57, 56, 61, 58, 63, 60, 66]
+        },
+        {
+          name: 'Revenue',
+          data: [ 101, 98, 87, 105, 91, 114, 94]
+        },
+        {
+          name: 'Free Cash Flow',
+          data: [36, 26, 45, 48, 52, 53, 41]
+        }
+      ],
+      chart: {
+        type: 'bar',
+        height: 350,
+        toolbar: {
+          show:false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent']
+      },
+      xaxis: {
+        categories: ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+      }
+    }
+  }
 }
