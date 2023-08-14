d3b74ef326887de580cfa4fc7e178845d087412d
siacerola <si.acerola@gmail.com>
2023-08-14 14:19:14 +0700

    remove template color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,9 @@ module.exports = {
     "./src/**/*.{html,js}"
   ],
   theme: {
-    colors: {
-      'primary-blue': '#3F72AF',
-      'secondary-blue': '#112D4E',
-      'secondary-slate': '#DBE2EF',
-      'primary-slate':'#F9F7F7'
-    },
-    extend: {},
+    extend: {
+
+    }
   },
   plugins: [
   ]
