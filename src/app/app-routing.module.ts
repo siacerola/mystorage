396aa79dff9056d0e281c49dc158844c96049329
sihar <si.acerola@gmail.com>
2023-10-05 15:58:59 +0700

    routing pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ItemListComponent } from './pages/item-list/item-list.component';
 import { ChartlineComponent } from './pages/chartline/chartline.component';
+import { SettingComponent } from './pages/setting/setting.component';
+import { PurchasingComponent } from './pages/purchasing/purchasing.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'dashboard/summary', component:  ChartlineComponent},
-  { path: 'dashboard/item-list', component: ItemListComponent }
+  { path: 'dashboard/summary', component: ChartlineComponent },
+  { path: 'dashboard/item-list', component: ItemListComponent },
+  { path: 'dashboard/settings', component: SettingComponent },
+  { path: 'dashboard/purchasing', component: PurchasingComponent }
+
 ];
 
 @NgModule({
