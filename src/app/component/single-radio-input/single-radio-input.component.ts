899114ec973eb004247de1c4eaad0ef7c1af20b1
sihar <si.acerola@gmail.com>
2023-10-07 09:57:20 +0700

    create component single radio button

---
+++ src/app/component/single-radio-input/single-radio-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-radio-input',
+  templateUrl: './single-radio-input.component.html',
+  styleUrls: ['./single-radio-input.component.css']
+})
+export class SingleRadioInputComponent {
+
+}
